1b2327da50196caf97f710e5dc64a5d2cce59865
DiegoRoney <diego-roney@outlook.com>
2022-08-03 14:58:28 -0300

    ok!

--- cronometro.js
+++ cronometro.js
@@ -1,21 +1,6 @@
-var deadline = new Date("Mar 3, 2024 15:00:00").getTime();
-var x = setInterval(function() {
-var now = new Date().getTime();
-var t = deadline - now;
-var days = Math.floor(t / (1000 * 60 * 60 * 24));
-var hours = Math.floor((t%(1000 * 60 * 60 * 24))/(1000 * 60 * 60));
-var minutes = Math.floor((t % (1000 * 60 * 60)) / (1000 * 60));
-var seconds = Math.floor((t % (1000 * 60)) / 1000);
-document.getElementById("cronometro").innerHTML = days + "d " 
-+ hours + "h " + minutes + "m " + seconds + "s ";
-    if (t < 0) {
-        clearInterval(x);
-        document.getElementById("cronometro").innerHTML = "FIM";
-    }
-}, 1000);
 
 //
-var deadline = new Date("Mar 3, 2024 15:00:00").getTime();
+var deadline = new Date("Dec 3, 2022 23:00:00").getTime();
   
 var x = setInterval(function() {
   
@@ -31,7 +16,7 @@ document.getElementById("minute").innerHTML = minutes;
 document.getElementById("second").innerHTML =seconds; 
 if (t < 0) {
         clearInterval(x);
-        document.getElementById("cronometro").innerHTML = "TIME UP";
+        document.getElementById("cronometro").innerHTML = "FELIZ NATAL";
         document.getElementById("day").innerHTML ='0';
         document.getElementById("hour").innerHTML ='0';
         document.getElementById("minute").innerHTML ='0' ; 
